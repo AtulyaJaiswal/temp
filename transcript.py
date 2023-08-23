ebf59985d402aeb0afb9faccdf91643270b463c4
prakhar191 <gkprakhar@gmail.com>
2023-08-23 21:38:09 +0530

    hh

--- transcript.py
+++ transcript.py
@@ -4,4 +4,4 @@ import replicate
 
 # audio_file= open(r"C:\Users\atuly\Downloads\test.mp3", "rb")
 # transcript = openai.Audio.transcribe("whisper-1", audio_file)
-replicate.client(api_token="de6b7412d8592848be2b55c552dad98c71109a4a")
+replicate.client(api_token="de6b7412d8592848be2b55c552dad98c71109a4a")fyufyfff
